f7eb6506cd149f2d65dddebc054f9f82f81cd630
Willie Hung <sungjiehung@gmail.com>
2021-07-22 12:26:37 +0800

    Update aggregate_function.sql

--- SQL/aggregate_function.sql
+++ SQL/aggregate_function.sql
@@ -1,14 +1,21 @@
-==
+/* AVG(), COUNT() */
 SELECT AVG(UnitPrice) AS avg_price
 FRROM products
 
-==
 SELECT COUNT (*) AS
 total customers
 FROM Customers;
 
-==
 SELECT COUNT(CustomerID) AS
 total customers
 FROM Customers;
+
+/* MAX(), MIN(), SUM() */
+
+/* DISTINCT() */
+/* IF DISTINCT is not specified, all is assumed. */
+SELECT COUNT(DISTINCT CustomerID)
+FROM Customers
+
+
 
