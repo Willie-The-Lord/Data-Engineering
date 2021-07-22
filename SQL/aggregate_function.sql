fa7b71ad520abb1e5f8f13bb9e94e0507c31405e
Willie Hung <sungjiehung@gmail.com>
2021-07-22 12:21:48 +0800

    Create aggregate_function.sql

---
+++ SQL/aggregate_function.sql
@@ -0,0 +1,14 @@
+==
+SELECT AVG(UnitPrice) AS avg_price
+FRROM products
+
+==
+SELECT COUNT (*) AS
+total customers
+FROM Customers;
+
+==
+SELECT COUNT(CustomerID) AS
+total customers
+FROM Customers;
+
