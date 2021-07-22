a120b395b06044ebf3e848abd127152ff6bb2028
Willie Hung <sungjiehung@gmail.com>
2021-07-22 12:37:31 +0800

    Update grouping.sql

--- SQL/grouping.sql
+++ SQL/grouping.sql
@@ -1,5 +1,21 @@
+/* GROUP BY */
 SELECT
 Region
 ,COUNT(CustomerID) AS total_customers
 FROM Customers
 GROUP BY Region;
+
+/*HAVING*/
+SELECT
+CustomerID
+,COUNT (*) AS orders
+FROM Orders
+GROUP BY CustomerID
+HAVING COUNT (*) >= 2; 
+
+/* REMINDER!!
+Remember that WHERE filters before the data is grouped;
+and then, HAVING filters after the data is grouped. */
+
+
+
