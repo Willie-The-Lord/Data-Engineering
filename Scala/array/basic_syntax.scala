b796656c0b4a002cea4de79d93b6bc8917ecd037
Willie Hung <sungjiehung@gmail.com>
2021-07-23 22:25:15 +0800

    Create basic_syntax.scala

---
+++ Scala/array/basic_syntax.scala
@@ -0,0 +1,10 @@
+//Declaring an array variable
+
+var z:Array[String] = new Array[String](3)
+
+var z = new Array[String](3)
+
+//Command
+z(0) = "Zara"; z(1) = "Nuha"; z(4/2) = "Ayan"
+
+var z = Array("Zara", "Nuha", "Ayan")
