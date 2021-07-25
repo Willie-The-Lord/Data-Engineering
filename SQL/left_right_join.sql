744ce3c302bcf5e0f0e701fc9f0fd9b41beb9fcc
Willie Hung <sungjiehung@gmail.com>
2021-07-25 21:24:03 +0800

    Update left_right_join.sql

--- SQL/left_right_join.sql
+++ SQL/left_right_join.sql
@@ -1,4 +1,16 @@
+/* Left Join */
 SELECT C.CustomerName, O.OrderID
 FROM Customers C
 LEFT JOIN Orders O ON C.CustomerID = O.CustomerID
 ORDER BY C.CustomerName;
+
+/* Right Join (More Complex--Not Support by SQLite)*/
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID
+
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+ORDER BY Customers.CustomerName;
