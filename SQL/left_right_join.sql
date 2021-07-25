3a4efd703c5f7804dbd473f8721a3201d3f05365
Willie Hung <sungjiehung@gmail.com>
2021-07-25 21:24:23 +0800

    Update left_right_join.sql

--- SQL/left_right_join.sql
+++ SQL/left_right_join.sql
@@ -10,6 +10,7 @@ FROM Orders
 RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
 ORDER BY Orders.OrderID
 
+/* Full Outer Join */
 SELECT Customers.CustomerName, Orders.OrderID
 FROM Customers
 FULL OUTER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
