d4e4f405e4d103b41b9464d1e08b1742d4eefa94
Willie Hung <sungjiehung@gmail.com>
2021-07-26 23:57:26 +0800

    Create cross_joins.sql

---
+++ SQL/cross_joins.sql
@@ -0,0 +1,10 @@
+SELECT table_column1, table_column2...
+FROM table_name1
+CROSS JOIN table_name2;
+
+SELECT table_column1, table_column2...
+FROM table_name1, table_name2;
+
+SELECT table_column1, table_column2...
+FROM table_name1
+JOIN table_name2;
